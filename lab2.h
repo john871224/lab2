7a09797ee5fa821c88cc95322d1b771451764b45
john871224 <36880287+john871224@users.noreply.github.com>
2018-03-28 22:25:34 +0800

    Add files via upload

---
+++ lab2.h
@@ -0,0 +1,14 @@
+
+#include<iostream>
+#include<string>
+class Lab2
+{
+     public:
+   float calculate(int,int);
+   std::string determine();
+   
+private:
+   int a,b;
+   float BMI;
+   std::string category;
+};
